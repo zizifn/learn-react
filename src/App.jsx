7cb8bd7019c788e5755536404bb980f0367b9767
zizifn <1803942+zizifn@users.noreply.github.com>
2025-02-27 20:45:18 +0800

    Add CoreConcept component and data for core concepts section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import reactImg  from './assets/react-core-concepts.png';
+import reactImg from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data.js';
+
 
 
 const reactDescriptions = ["Fundament", "Core", "Crucial"];
@@ -8,7 +10,7 @@ function getRandomInt(max) {
 }
 
 function Header() {
-  const  description = reactDescriptions[getRandomInt(reactDescriptions.length)];
+  const description = reactDescriptions[getRandomInt(reactDescriptions.length)];
   return <header>
     <img src={reactImg} alt="Stylized atom" />
     <h1>React Essentials</h1>
@@ -19,11 +21,29 @@ function Header() {
   </header>
 }
 
+function CoreConcept({img, title, description}) {
+
+  return (
+  <li>
+    <img src={img} alt="" srcset="" />
+    <h3>{title}</h3>
+    <p>{description}</p>
+  </li>)
+}
+
 function App() {
   return (
     <div>
-      <Header></Header>
+      <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core concepts</h2>
+          <ul>
+          <CoreConcept img={CORE_CONCEPTS[0].img} title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} />
+          <CoreConcept img={CORE_CONCEPTS[1].img} title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} />
+      </ul>
+
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
---
+++ src/data.js
@@ -0,0 +1,20 @@
+
+import componentImg from './assets/components.png';
+import configImg from './assets/config.png';
+
+const CORE_CONCEPTS = [
+{
+    title: "Component",
+    description: "Reusable building blocks of a React application.",
+    img: componentImg
+},
+{
+    title: "Config",
+    description: "Short for properties, these are how we pass data to components.",
+    img: configImg
+}
+]
+
+export {
+    CORE_CONCEPTS
+}
