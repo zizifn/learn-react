48d059b4de4c1cbd833cf6bf0b53a48eac6648cd
zizifn <1803942+zizifn@users.noreply.github.com>
2025-02-27 20:24:59 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import reactImg  from './assets/react-core-concepts.png';
+
+
+const reactDescriptions = ["Fundament", "Core", "Crucial"];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function Header() {
+  const  description = reactDescriptions[getRandomInt(reactDescriptions.length)];
+  return <header>
+    <img src={reactImg} alt="Stylized atom" />
+    <h1>React Essentials</h1>
+    <p>
+      {description} React concepts you will need for almost any app you are
+      going to build!
+    </p>
+  </header>
+}
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
