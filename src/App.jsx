5b7407bb4a75d5caa646274cf8529d3ab60829e0
zizifn <1803942+zizifn@users.noreply.github.com>
2025-03-02 23:47:34 +0800

    add it

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
-import Player from "./components/Player.jsx";
-import { TimerChallenge } from "./components/TimerChallenge.jsx";
 function App() {
   return (
     <>
-      <Player />
-      <div id="challenges">
-        <TimerChallenge title={"Easy"} targetTime={1}></TimerChallenge>
-        <TimerChallenge title={"Not Easy"} targetTime={5}></TimerChallenge>
-        <TimerChallenge title={"Tough"} targetTime={10}></TimerChallenge>
-        <TimerChallenge title={"PRO ONLY"} targetTime={15}></TimerChallenge>
-      </div>
+      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
     </>
   );
 }
