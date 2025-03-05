3a926956fe10e862718404e7b13c6e0f68e7fa6d
zizifn <1803942+zizifn@users.noreply.github.com>
2025-03-05 22:17:48 +0800

    init

---
+++ old/src-context/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+import Header from "./components/Header.jsx";
+import Shop from "./components/Shop.jsx";
+import { DUMMY_PRODUCTS } from "./dummy-products.js";
+import {
+  ShoppingCartState,
+  useShoppingCart,
+  ShoppingCartContext,
+} from "./store/ShoppingCart.jsx";
+
+function App() {
+  const ctxValue = useShoppingCart();
+  return (
+    <>
+      <ShoppingCartState value={ctxValue}>
+        <Header />
+        <Shop />
+      </ShoppingCartState>
+      {/* <ShoppingCartContext>
+        <Header />
+        <Shop />
+      </ShoppingCartContext> */}
+    </>
+  );
+}
+
+export default App;
--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,97 @@
 import { useState } from "react";
-
-import Header from "./components/Header.jsx";
-import Shop from "./components/Shop.jsx";
-import { DUMMY_PRODUCTS } from "./dummy-products.js";
-import {
-  ShoppingCartState,
-  useShoppingCart,
-  ShoppingCartContext,
-} from "./store/ShoppingCart.jsx";
+import { SideBar } from "./components/SideBar";
+import { InitPage } from "./components/InitPage";
+import { NewProject } from "./components/NewProject";
+import { ProjectDetail } from "./components/ProjectDetail";
 
 function App() {
-  const ctxValue = useShoppingCart();
+  const [projects, setProjects] = useState([]);
+  const [selectedProject, setSelectedProject] = useState(0);
+
+  const [isAdding, setIsAdding] = useState(false);
+
+  const noProjects = projects.length < 1;
+
+  const showProjectDetails = !noProjects && !isAdding;
+  let project = null;
+  if (showProjectDetails) {
+    project = projects[selectedProject];
+  }
+
+  function addTask(task) {
+    console.log("add task");
+    setProjects((old) => {
+      const newProjects = structuredClone(old);
+      if (!newProjects[selectedProject].tasks) {
+        newProjects[selectedProject].tasks = [];
+      }
+      newProjects[selectedProject].tasks.push(task);
+
+      return newProjects;
+    });
+  }
+
+  function clearTask(index) {
+    setProjects((old) => {
+      const clone = structuredClone(old);
+      if (!clone[selectedProject].tasks) {
+        clone[selectedProject].tasks = [];
+      }
+      const tasks = clone[selectedProject].tasks;
+      tasks.splice(index, 1); // Fixed: Added second parameter to remove only one item
+      return clone;
+    });
+  }
+
+  function selectProject(projectIndex) {
+    setSelectedProject(projectIndex);
+  }
+
+  function addingPorject() {
+    setIsAdding(true);
+  }
+
+  function cancelAddProject() {
+    setIsAdding(false);
+  }
+
+  /**
+   *
+   * @param {import("react").FormEvent} event
+   */
+  function addProject(event) {
+    console.log(event);
+    setProjects((old) => {
+      return [...old, Object.fromEntries(event.entries())];
+    });
+    setIsAdding(false);
+  }
+
   return (
     <>
-      <ShoppingCartState value={ctxValue}>
-        <Header />
-        <Shop />
-      </ShoppingCartState>
-      {/* <ShoppingCartContext>
-        <Header />
-        <Shop />
-      </ShoppingCartContext> */}
+      <div className="flex h-lvh">
+        <SideBar
+          projects={projects}
+          selectProject={selectProject}
+          addingPorject={addingPorject}
+        ></SideBar>
+        <div className="flex-2/3">
+          {isAdding ? (
+            <NewProject
+              cancelAddProject={cancelAddProject}
+              addProject={addProject}
+            ></NewProject>
+          ) : showProjectDetails ? (
+            <ProjectDetail
+              addTask={addTask}
+              clearTask={clearTask}
+              project={project}
+            ></ProjectDetail>
+          ) : (
+            <InitPage addingPorject={addingPorject}></InitPage>
+          )}
+        </div>
+      </div>
     </>
   );
 }
