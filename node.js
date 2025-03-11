cf77fe8287343ea912c0223850f607bb8678c277
zizifn <1803942+zizifn@users.noreply.github.com>
2025-03-11 12:42:55 +0800

    update it

--- node.js
+++ node.js
@@ -1,8 +1,27 @@
 
 
-function test(){
+const p1 = new Promise((resolve, rejct) => {
+    console.log("inside p1")
 
-    return Promise.resolve("111");
-}
+    setTimeout(() => {
+        console.log("resolve p1")
+        const p2 = new Promise((resolve, rejct) => {
+            console.log("inside p2")
+            setTimeout(() => {
+                console.log("resolve p2")
+                resolve("p2")
+            }, 1000)
+        })
+        resolve(p2)
+    }, 500)
+})
 
-const value = await test()
+p1.then(value => {
+    console.log("inside then")
+    console.log(value)
+    return Promise.resolve(2);
+}).then(value =>{
+    console.log("inside then2")
+    console.log(value)
+})
+
