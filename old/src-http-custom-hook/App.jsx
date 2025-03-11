e32dbca8b8e3161a5367ad702e85013f8e6f8fbb
zizifn <1803942+zizifn@users.noreply.github.com>
2025-03-11 12:44:58 +0800

    init form section

---
+++ old/src-http-custom-hook/App.jsx
@@ -0,0 +1,162 @@
+import { useRef, useState, useCallback, useEffect } from "react";
+
+import Places from "./components/Places.jsx";
+import Modal from "./components/Modal.jsx";
+import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
+import logoImg from "./assets/logo.png";
+import AvailablePlaces from "./components/AvailablePlaces.jsx";
+import React from "react";
+import { updateUserPlaces, fetchUserPlaces } from "./http.js";
+import ErrorMessage from "./components/Error.jsx";
+import { useFetch } from "./hooks/http-hook.js";
+function App() {
+  const selectedPlace = useRef();
+
+  const {
+    data: userPlaces,
+    isFetching,
+    error: fetchUserPlaceError,
+    setData
+  } = useFetch(fetchUserPlaces, []);
+
+  // const [userPlaces, setUserPlaces] = useState([]);
+  // const [isFetching, setIsFetching] = useState(false);
+  // const [fetchUserPlaceError, setfetchUserPlaceError] = useState("");
+
+  console.log("userPlaces", userPlaces);
+  const [error, setError] = useState(null);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  // useEffect(() => {
+  //   const abortController = new AbortController();
+  //   async function getUserPlace() {
+  //     setIsFetching(true);
+  //     setfetchUserPlaceError("");
+  //     try {
+  //       const userPlaces = await fetchUserPlaces(abortController.signal);
+  //       setfetchUserPlaceError("");
+  //       setUserPlaces(userPlaces.places);
+  //       setIsFetching(false);
+  //     } catch (error) {
+  //       console.log("app effect", error)
+  //       setIsFetching(false);
+  //       setfetchUserPlaceError(error.message || "failed to load user places");
+  //     }
+  //   }
+
+  //   getUserPlace();
+  //   return () => abortController.abort("abort due effect");
+  // }, []);
+
+  function handleStartRemovePlace(place) {
+    setModalIsOpen(true);
+    selectedPlace.current = place;
+  }
+
+  function handleStopRemovePlace() {
+    setModalIsOpen(false);
+  }
+
+  function handleError() {
+    setError(null);
+  }
+
+  async function handleSelectPlace(selectedPlace) {
+    setData((prevPickedPlaces) => {
+      if (!prevPickedPlaces) {
+        prevPickedPlaces = [];
+      }
+      if (prevPickedPlaces.some((place) => place.id === selectedPlace.id)) {
+        return prevPickedPlaces;
+      }
+      return [selectedPlace, ...prevPickedPlaces];
+    });
+
+    // const {error, data} = useFetch(()=>{
+    //   return updateUserPlaces([selectedPlace, ...userPlaces])
+    // })
+    // if(error){
+    //   setError(error || '');
+    //   setData(userPlaces);
+    // }
+    try {
+      
+      const result = await updateUserPlaces([selectedPlace, ...userPlaces]);
+    } catch (error) {
+      setError(error.message);
+      setData(userPlaces);
+    }
+  }
+
+  const handleRemovePlace = useCallback(
+    async function handleRemovePlace() {
+      setData((prevPickedPlaces) =>
+        prevPickedPlaces.filter(
+          (place) => place.id !== selectedPlace.current.id,
+        ),
+      );
+
+      try {
+        const requestBody = userPlaces.filter(
+          (place) => place.id !== selectedPlace.current.id,
+        );
+        const result = await updateUserPlaces(requestBody);
+      } catch (error) {
+        setError(error.message);
+        setData(userPlaces);
+      }
+
+      setModalIsOpen(false);
+    },
+    [userPlaces, setData],
+  );
+
+  return (
+    <>
+      <Modal open={error} onClose={handleError}>
+        <ErrorMessage
+          title={"An error occurred"}
+          message={error}
+          onConfirm={handleError}
+        ></ErrorMessage>
+      </Modal>
+      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
+        <DeleteConfirmation
+          onCancel={handleStopRemovePlace}
+          onConfirm={handleRemovePlace}
+        />
+      </Modal>
+
+      <header>
+        <img src={logoImg} alt="Stylized globe" />
+        <h1>PlacePicker</h1>
+        <p>
+          Create your personal collection of places you would like to visit or
+          you have visited.
+        </p>
+      </header>
+      <main>
+        {fetchUserPlaceError && (
+          <ErrorMessage
+            title={"An error oaccurred"}
+            message={fetchUserPlaceError}
+          ></ErrorMessage>
+        )}
+        <Places
+          title="I'd like to visit ..."
+          fallbackText="Select the places you would like to visit below."
+          places={userPlaces}
+          isLoading={isFetching}
+          loadingText={"loading user places"}
+          onSelectPlace={handleStartRemovePlace}
+        />
+
+        <AvailablePlaces
+        onSelectPlace={handleSelectPlace}
+        />
+      </main>
+    </>
+  );
+}
+
+export default App;
--- old/src-http-custom-hook/main.jsx
+++ old/src-http-custom-hook/main.jsx
@@ -8,3 +8,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+
+//     <App />
+// )
+
